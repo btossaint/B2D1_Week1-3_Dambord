fb1e53adbc8eacb9e6b42ce5a44c47460aa73bbb
tosSaint <bob.tossaint@zuyd.nl>
2016-09-08 12:46:42 +0200

    Add project files.

---
+++ B2D1_Dambord_001/Model/Dambord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2D1_Dambord_001.Model
+{
+    public class Dambord
+    {
+        private int steen;
+        private List<int> dambordBord;
+
+        public int Steen
+        {
+            get { return steen; }
+            set { steen = value; }
+        }
+
+        public List<int> DambordBord
+        {
+            get { return dambordBord; }
+            set { dambordBord = value; }
+        }
+
+        public Dambord ()
+        {
+            creeer_dambord();
+            
+        }
+
+        private void creeer_dambord()
+        {
+            dambordBord = new List<int>() ;
+            for (int i = 0; i < 100; i++)
+            {
+                dambordBord.Add(0);
+            }
+            steen = 1;
+            setSteen(steen, 1);
+        }
+
+        public void setSteen(int plaats, int waarde)
+        {
+            steen = plaats;
+            dambordBord[steen-1] = waarde;            // -1 omdat er een offset is van 0!
+        }
+
+        public void toonRechts()
+        {
+            bool Klaar = false;
+            int i = steen;             
+            while (!Klaar)
+            {
+                if (i%10 != 0)
+                {
+                    Console.Write(dambordBord[i]);
+                }
+                else
+                {
+                    Klaar = true;
+                }
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+
+        public void toonLinks()
+        {            
+            int i = (steen / 10) * 10 ; // steen afronden op heel getal bijv, 42 / 10 = 4 * 10 = 40
+
+            while (i < steen-1) // -1 steen zelf niet meenemen
+            {
+                Console.Write(dambordBord[i]);
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+
+        public void toonBord()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if ((i+1)%10 == 0)
+                {
+                    Console.WriteLine(dambordBord[i]);
+                }
+                else
+                {
+                    Console.Write(dambordBord[i]);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ B2D1_Dambord_001/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B2D1_Dambord_001.Model;
+
+namespace B2D1_Dambord_001
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            Dambord Dbord = new Dambord();
+            Dbord.toonBord();
+            Dbord.setSteen(33,1);
+            Dbord.toonBord();
+            Dbord.setSteen(44, 1);
+            Dbord.setSteen(42, 1);
+            Dbord.toonBord();
+            Dbord.toonRechts();
+            Dbord.toonLinks();
+        }
+    }
+}
